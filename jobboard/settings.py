040878c9a610106a01d55a11f7a7766551277a3a
Rajat Dev Sharma <rajatdevsharma@gmail.com>
2022-05-12 18:49:28 +0530

    Hidding the secret key as an ENV Variable

--- jobboard/settings.py
+++ jobboard/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ky!_@y2pa#%j%63xb*evt5=t2hswu1h(a*sc9_=v+*#4772@@9'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
